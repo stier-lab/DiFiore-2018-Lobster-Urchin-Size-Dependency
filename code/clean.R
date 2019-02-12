b11ed50ffbbe47cf188567cde59fc94b4ce8e9fb
bartdifiore <bdifiore@ucsb.edu>
2019-02-11 22:41:20 -0800

    Add data, setup.R, and clean.R files

---
+++ code/clean.R
@@ -0,0 +1,25 @@
+##############################################################################
+## Clean raw csv file to prepare for fitting functional responses
+##############################################################################
+
+df <- read.table("data/raw/loburc_raw.csv", header = T, sep = ",")
+
+df <- df %>% 
+  filter(finished == "finished", discard != "yes") %>%
+  select(-order, - repeat., -last_molt, -sex, -finished, -starve_hours, -food_remaining, -discard, -notes)
+
+
+###############################################################################
+## Table to visualize replication
+###############################################################################
+
+table(df$id, df$num_offered) # So there was obviously an issue and some lobsters did not recieve all of the densities. Not sure of the implications of this...
+
+
+table(df$class, df$num_offered, df$treatment) # We have a problem with replication in the jumbo size class which we knew was going to happen.
+
+###############################################################################
+## Write csv
+###############################################################################
+
+write.table(df, "data/loburc_cleaned.csv", sep = ",", quote = F, row.names = F)
